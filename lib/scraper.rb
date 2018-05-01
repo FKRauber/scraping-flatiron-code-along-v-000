4cd3f2b6e06f157a9be55a067513f1e964fd8f3c
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-05-01 16:38:20 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,14 +20,14 @@ class Scraper
     def get_page      # instance method will be responsible for using Nokogiri and open-uri to grab the entire HTML document from the web page.
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
 
-      doc.css(".post").each do |post|
-        course = Course.new
-        course.title = post.css("h2").text
-        course.schedule = post.css(".date").text
-        course.description = post.css("p").text
+      #doc.css(".post").each do |post|
+      #  course = Course.new
+      #  course.title = post.css("h2").text
+      #  course.schedule = post.css(".date").text
+      #  course.description = post.css("p").text
 
         #binding.pry
-      end
+      #end
     end
     def get_courses   # instance method will be responsible for using a CSS selector to grab all of the HTML elements that contain a course.
 
